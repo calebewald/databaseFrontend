4cf65f2f6b4e680c5f0d415257e32a9be38e58d1
calebewald <138894638+calebewald@users.noreply.github.com>
2024-05-31 09:26:35 -0400

    replaced some AddButton fields with drop down menus

--- src/AddButton.jsx
+++ src/AddButton.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import axios from 'axios';
 
 const AddButton = ({ tableData, setTableData, currentModifier, setCurrentModifier }) => {
@@ -9,16 +9,24 @@ const AddButton = ({ tableData, setTableData, currentModifier, setCurrentModifie
     const categoryInput = useRef(null);
     const aisle_numberInput = useRef(null);
 
+    const [error, setError] = useState("");
+
     /*Opens or closes the form*/
     const toggleAddButton = (e) => {
         e.preventDefault(); // don't know what this does but chatGPT keeps putting it in event handlers
         setCurrentModifier(currentModifier == "AddButton" ? "" : "AddButton");
+        setError("");
     }
 
     /* function executed when "submit" is clicked. Right now it tries to add the given
     information to the database */
     const handleSubmit = async (tableData, setTableData) => {
-        // 
+        // TD: add guard clauses, right now any value is accepted
+        if (isNaN(Number(priceInput.current.value))) {
+            setError("Invalid Entry. Price is not a number");
+            return;
+        }
+
         var newID = 1; var IDArray = [];
         for (var i = 0; i < tableData.length; i++) {
             IDArray.push(tableData[i].ID); // array of all current IDs
@@ -26,7 +34,7 @@ const AddButton = ({ tableData, setTableData, currentModifier, setCurrentModifie
         while (IDArray.includes(newID)) {
             newID += 1; // newID will be smallest integer greater than -1 that isn't an ID already
         }
-        // TD: add guard clauses, right now any value is accepted
+
         const newData = {
             ID: newID,
             Name: nameInput.current.value,
@@ -52,13 +60,36 @@ const AddButton = ({ tableData, setTableData, currentModifier, setCurrentModifie
 
     /*The form itself. This is conditionally rendered in the component based on the state of currentModifier */
     const form = (
-        <ul>
-            <li><label>Name: <input type="text" ref={nameInput} /></label></li>
-            <li><label>Price: <input type="text" ref={priceInput} /></label></li>
-            <li><label>Category: <input type="text" ref={categoryInput} /> </label></li>
-            <li><label>Aisle Number: <input type="text" ref={aisle_numberInput} /></label></li>
-            <button type="submit" onClick={() => handleSubmit(tableData, setTableData)}>Submit</button>
-        </ul>
+        <div>
+            <ul>
+                <li><label>Item Name: <input type="text" ref={nameInput} /></label></li>
+                <li><label>Price: <input type="text" ref={priceInput} /></label></li>
+                <li><label value="options">Category: </label>
+                    <select id="options" ref={categoryInput} name="options">
+                        <option value="Dairy">Dairy</option>
+                        <option value="Produce">Produce</option>
+                        <option value="Meat">Meat</option>
+                        <option value="Basic">Basic</option>
+                        <option value="Drink">Drink</option>
+                        <option value="Grain">Grain</option>
+                    </select></li>
+                <li><label value="options">Aisle Number: </label>
+                    <select id="options" ref={aisle_numberInput} name="options">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                        <option value="8">8</option>
+                        <option value="9">9</option>
+                    </select></li>
+
+                <button type="submit" onClick={() => handleSubmit(tableData, setTableData)}>Submit</button>
+            </ul>
+            {error != "" && <div>Error: Please check submission fields</div>}
+        </div>
     );
 
     return (
@@ -70,5 +101,4 @@ const AddButton = ({ tableData, setTableData, currentModifier, setCurrentModifie
         </div>
     );
 };
-
 export default AddButton;
