77c29327e4cdbddfd954aeb744e61ffc0c505c2c
calebewald <138894638+calebewald@users.noreply.github.com>
2024-06-03 09:15:20 -0400

    config changes

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0', // Ensure the server listens on 0.0.0.0
-    port: 8080, // Optional: You can set the port to 3000 or another value
-  },
-  build: {
-    outDir: 'dist',
-  },
-});
+})
